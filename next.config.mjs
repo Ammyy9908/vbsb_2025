646ee4df5d5e5e90b03c366d58994d17e047a50e
ammyy9908 <sumit@kodnest.com>
2025-01-08 02:17:20 +0530

    es lint disabled fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,9 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'images.ctfassets.net'],
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  output: 'standalone',
+  poweredByHeader: false,
+  generateEtags: false,
 }
 
 export default nextConfig
