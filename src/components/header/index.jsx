315563dcd4e2b6a49a641c000d27fb63f632dd9e
ammyy9908 <sumit@kodnest.com>
2025-01-08 03:15:02 +0530

    anchor tag replaced with link

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react'
 import { MdOutlineMenu } from "react-icons/md";
 import { MdClose } from "react-icons/md";
-
-
+import Link from 'next/link';
 
 function NavLink({path, label}) {
     return (
         <li className='text-lg font-medium text-[#4F2D7F]'>
-            <a href={path} className='inline-block py-2 px-4 rounded-lg bg-white bg-opacity-50 backdrop-blur-sm hover:bg-purple-600 hover:text-white transition-colors duration-200'>{label}</a>
+            <Link href={path} className='inline-block py-2 px-4 rounded-lg bg-white bg-opacity-50 backdrop-blur-sm hover:bg-purple-600 hover:text-white transition-colors duration-200'>{label}</Link>
         </li>
     )
 }
@@ -19,9 +18,9 @@ function Header() {
     <div className='w-full'>
    <div className='bg-white w-full py-6 sticky top-0 z-50 shadow-md bg-opacity-50 backdrop-blur-sm'>
      <div className='flex justify-between items-center w-[85%] mx-auto'>
-        <a href="/" className='text-lg font-bold'>
+        <Link href="/" className='text-lg font-bold'>
             <span>VBSB</span>
-        </a>
+        </Link>
         <button className='md:hidden text-black font-bold text-xl' onClick={() => setMenuOpen(!menuOpen)}>{menuOpen ? <MdClose /> : <MdOutlineMenu />}</button>
         <nav className='hidden md:flex justify-center items-center'>
             <ul className='flex gap-4 items-center'>
